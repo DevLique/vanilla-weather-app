0f2938a86f01d417b0d6a63b391beb0d9fcc897b
Lique007 <angeliquehilario019@gmail.com>
2023-07-05 14:41:37 +0200

    updated the current temperature

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-alert("ice cream chillin chillin");
+function displayTemperature(response){
+    
+    let temperatureElement = document.querySelector("#temperature");
+    let cityElement=document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
+    windElement.innerHTML = Math.round(response.data.wind.speed);
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    cityElement.innerHTML = response.data.name;
+    descriptionElement.innerHTML = response.data.weather[0].description;
+    humidityElement.innerHTML = response.data.main.humidity;
+}
+
+let key = "f5029b784306910c19746e40c14d6cd3";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${key}&units=metric`;
+axios.get(apiUrl).then(displayTemperature);
+
+//  api.openweathermap.org/data/2.5/weather?q=New York&appid=f5029b784306910c19746e40c14d6cd3&units=metric
